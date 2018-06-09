305edf1cfbe9aba04d50c4ec8f653a8c441c46f8
Lenin Meza <merolhack@gmail.com>
2018-06-09 14:14:07 -0500

    agregamos solucion final att: Citlali, Lenin y Alberto

--- src/main.js
+++ src/main.js
@@ -2,15 +2,45 @@
 import 'bootstrap';
 // Load the SASS styles
 import './main.scss';
-
-
+// Load the HTML
 import nav from './partials/nav.html';
-import jumbotron from './partials/jumbotron.html';
 import mainContent from './partials/main-content.html';
+import menuContent from './partials/menu-content.html';
+import ordersContent from './partials/orders-content.html';
+import myAccount from './partials/my-account.html';
 import footer from './partials/footer.html';
-
-
+// Create and append the HTML to body
 const root = document.createElement("div");
-root.className = 'content-coffee';
-root.innerHTML = nav + jumbotron + mainContent + footer;
-document.body.appendChild(root);
+const dummy = document.createElement("div");
+dummy.className = 'content';
+
+ root.className = 'content-site';
+ 
+
+
+// Show the content methods
+window.showIndex = function() {
+    dummy.innerHTML = nav + mainContent + footer;
+    root.appendChild(dummy);
+    document.body.appendChild(root);
+};
+window.showMenu = function() {
+    dummy.innerHTML = nav + menuContent + footer;
+    root.appendChild(dummy);
+    document.body.appendChild(root);
+};
+window.showOrders = function() {
+    dummy.innerHTML = nav + ordersContent + footer;
+    root.appendChild(dummy);
+    document.body.appendChild(root);
+};
+window.showMyAccount = function() {
+    dummy.innerHTML = nav + myAccount + footer;
+    root.appendChild(dummy);
+    document.body.appendChild(root);
+};
+// Show index content
+window.showIndex();
+
+// const menuItem = document.getElementById('menu-item');
+// menuItem.addEventListener
