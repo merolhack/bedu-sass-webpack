f12d18cfc3f40da11b9da8ca2edf7a4ed637c4a1
Lenin Meza <merolhack@gmail.com>
2018-06-09 12:03:04 -0500

    :sparkles: SASS and HTML imported

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ import './main.scss';
 
 
 import nav from './partials/nav.html';
+import jumbotron from './partials/jumbotron.html';
+import mainContent from './partials/main-content.html';
+import footer from './partials/footer.html';
+
 
 const root = document.createElement("div");
-root.innerHTML = nav;
+root.className = 'content-coffee';
+root.innerHTML = nav + jumbotron + mainContent + footer;
 document.body.appendChild(root);
